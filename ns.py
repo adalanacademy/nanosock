cdc9504d149d96bea5958b7bd5e4bd461676691c
Jay Butera <buterajay@protonmail.com>
2023-04-13 21:59:30 -0500

    save prog

--- ns.py
+++ ns.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 import socket
 import struct
@@ -11,6 +12,7 @@ parser = argparse.ArgumentParser(description="A socket server/client that runs a
 parser.add_argument("command", nargs='?', help="The command to run, including arguments and flags.")
 parser.add_argument("--port", type=int, default=12345, help="The port number to bind the server to or connect to. (default: 12345)")
 parser.add_argument("--debug", type=bool, default=False, help="Print stdout of the subprocess")
+parser.add_argument("--parse", type=bool, default=False, help="Read input until '###' is parsed.")
 parser.add_argument("--connect", "-c", type=str, help="The address to connect as a client.")
 args = parser.parse_args()
 
@@ -27,52 +29,67 @@ def encode_string_with_length(s: str) -> bytes:
     # Return the combined binary data
     return length_binary + utf8_binary
 
-def recv_msg(sock) -> str:
-    length_binary = sock.recv(4)
+async def read_until_chars(src, chars) -> str:
+    output = []
+    while True:
+        char = src.read(1)
+        #char = await process.stdout.read(1)
+        #if not char:
+        #    break
+        output.append(char)
+        #if "".join(output[-len(chars):]).decode() == chars:
+        if "".join(output[-len(chars):]) == chars:
+            break
+    return "".join(output)
+
+async def recv_msg(reader) -> str:
+    #length_binary = sock.recv(4)
+    length_binary = await reader.read(4)
     # Extract the 32-bit unsigned integer representing the length
     length = struct.unpack('>I', length_binary)[0]
     # Extract the UTF-8 binary data
-    utf8_binary = sock.recv(length)
+    #utf8_binary = sock.recv(length)
+    utf8_binary = await reader.read(length)
     return utf8_binary.decode('utf-8')
 
 # Function to handle communication with the client.
-def handle_client(client_socket, client_address):
+async def handle_client(reader, writer):
     # Spawn the subprocess with the provided command.
     process = subprocess.Popen(args.command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
 
     # Function to read from the process and send data to the client.
-    def forward(src, dst):
+    async def forward(src, writer):
         while True:
             #data = src.readline()
-            data = src.readline()
+            data = await read_until_chars(src, '◊')
             if data:
-                #dst.sendall(data.encode('utf-8'))
                 if debug:
                     print(data)
-                dst.sendall(encode_string_with_length(data))
+                #dst.sendall(encode_string_with_length(data))
+                writer.write(encode_string_with_length(data))
+                await writer.drain()
             else:
+                writer.close()
                 break
 
     # Forward stdout and stderr of the subprocess to the client.
-    stdout_thread = threading.Thread(target=forward, args=(process.stdout, client_socket))
-    stderr_thread = threading.Thread(target=forward, args=(process.stderr, client_socket))
-
-    stdout_thread.start()
-    stderr_thread.start()
+    stdout_task = asyncio.create_task(forward(process.stdout, writer))
+    stderr_task = asyncio.create_task(forward(process.stderr, writer))
 
     # Read from the client and send data to the subprocess.
     while True:
-        #data = client_socket.recv(1024)
-        data = recv_msg(client_socket)
+        print('await msg from telegram')
+        data = await recv_msg(reader)
+        print(f'data recvd: {data}')
         if data:
             process.stdin.write(data)
             process.stdin.flush()
         else:
             break
 
+    await stdout_task
+    await stderr_task
     process.terminate()
-    stdout_thread.join()
-    stderr_thread.join()
 
 def run_client(address, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,7 +97,7 @@ def run_client(address, port):
 
     try:
         while True:
-            user_input = input() + '\n'
+            user_input = input() + '◊'
             if not user_input:
                 break
 
@@ -92,27 +109,38 @@ def run_client(address, port):
 
 # Sexy global var
 debug = args.debug
+parse = args.parse
 
-if args.connect:
-    run_client(args.connect, args.port)
-else:
-    if not args.command:
-        print("Error: command is required when running in server mode")
-        sys.exit(1)
+async def main():
+    if args.connect:
+        run_client(args.connect, args.port)
+    else:
+        if not args.command:
+            print("Error: command is required when running in server mode")
+            sys.exit(1)
 
-    # Create a socket server.
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Create a socket server.
+        #server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server = await asyncio.start_server(
+            handle_client, '127.0.0.1', args.port)
 
-    # Bind the server to an address and port.
-    server_address = ('localhost', args.port)
-    server_socket.bind(server_address)
+        async with server:
+            await server.serve_forever()
+        # Bind the server to an address and port.
+        #server_address = ('localhost', args.port)
+        #server_socket.bind(server_address)
 
-    # Listen for incoming connections.
-    server_socket.listen(1)
+        # Listen for incoming connections.
+        #server_socket.listen(1)
 
-    # Accept and handle connections.
-    while True:
-        client_socket, client_address = server_socket.accept()
-        client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_handler.start()
+        # Accept and handle connections.
+        while True:
+            client_socket, client_address = server_socket.accept()
+            #client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_handler = threading.Thread(target=asyncio.run(handle_client(client_socket, client_address)))
+            #client_task = asyncio.create_task(handle_client(client_socket, client_address))
+            #asyncio.run(client_
+            client_handler.start()
 
+if __name__ == '__main__':
+    asyncio.run(main())
